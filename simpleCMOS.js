a3339d922ae6e6d3fa72e71966229f4ed59fd5bf
Robert Rypuła <robert.rypula@gmail.com>
2014-10-30 00:25:41 +0100

    2013-08-06_0814

--- simpleCMOS.js
+++ simpleCMOS.js
@@ -771,6 +771,97 @@ function setCanvasOpacity(obj)
         $('#canvas-levels').removeClass('transparent');
 }
 
+var keys = {
+    KEY_ESCAPE       : 0,
+    KEY_GRAVE_ACCENT : 1
+}
+var keysState =
+[
+{ keyCode: 0, name: "KEY_ESCAPE", pressed: false },
+{ keyCode: 1, name: "KEY_GRAVE_ACCENT", pressed: false },
+];
+
+var keyboardEventQueue;
+var lastPressedKey;
+var lastPressedKeyTime;
+var keyboardEvent = 
+{
+    keyCode: 0, name: "KEY_ESCAPE", type: 0 /* 0: relased, 1: pressed, 2: repeated */
+}
+
+keysState[keys.KEY_ESCAPE].pressed;
+
+
+
+/*
+                                        00. KEY_ESCAPE
+                <div class="key key-01">01. KEY_GRAVE_ACCENT</div>
+                <div class="key key-02">02. KEY_1</div>
+                <div class="key key-03">03. KEY_2</div>
+                <div class="key key-04">04. KEY_3</div>
+                <div class="key key-05">05. KEY_4</div>
+                <div class="key key-06">06. KEY_5</div>
+                <div class="key key-07">07. KEY_6</div>
+                <div class="key key-08">08. KEY_7</div>
+                <div class="key key-09">09. KEY_8</div>
+                <div class="key key-10">10. KEY_9</div>
+                <div class="key key-11">11. KEY_0</div>
+                <div class="key key-12">12. KEY_MINUS</div>
+                <div class="key key-13">13. KEY_EQUALS</div>
+                <div class="key key-14">14. KEY_BACKSPACE</div>
+                <div class="key key-15">15. KEY_INSERT</div>
+                <div class="key key-16">16. KEY_HOME</div>
+                <div class="key key-17">17. KEY_PAGE_UP</div>
+                <div class="key key-18">18. KEY_TAB</div>
+                <div class="key key-19">19. KEY_Q</div>
+                <div class="key key-20">20. KEY_W</div>
+                <div class="key key-21">21. KEY_E</div>
+                <div class="key key-22">22. KEY_R</div>
+                <div class="key key-23">23. KEY_T</div>
+                <div class="key key-24">24. KEY_Y</div>
+                <div class="key key-25">25. KEY_U</div>
+                <div class="key key-26">26. KEY_I</div>
+                <div class="key key-27">27. KEY_O</div>
+                <div class="key key-28">28. KEY_P</div>
+                <div class="key key-29">29. KEY_SQUARE_BRACKET_LEFT</div> 
+                <div class="key key-30">30. KEY_SQUARE_BRACKET_RIGHT</div>
+                <div class="key key-31">31. KEY_BACKSLASH</div>
+                <div class="key key-32">32. KEY_DELETE</div>
+                <div class="key key-33">33. KEY_END</div>
+                <div class="key key-34">34. KEY_PAGE_DOWN</div>
+                <div class="key key-35">35. KEY_CAPS_LOCK</div>
+                <div class="key key-36">36. KEY_A</div>
+                <div class="key key-37">37. KEY_S</div>
+                <div class="key key-38">38. KEY_D</div>
+                <div class="key key-39">39. KEY_F</div>
+                <div class="key key-40">40. KEY_G</div>
+                <div class="key key-41">41. KEY_H</div>
+                <div class="key key-42">42. KEY_J</div>
+                <div class="key key-43">43. KEY_K</div>
+                <div class="key key-44">44. KEY_L</div>
+                <div class="key key-45">45. KEY_SEMICOLON</div>
+                <div class="key key-46">46. KEY_APOSTROPHE</div>
+                <div class="key key-47">47. KEY_ENTER</div>
+                <div class="key key-48">48. KEY_SHIFT</div>
+                <div class="key key-49">49. KEY_Z</div>
+                <div class="key key-50">50. KEY_X</div>
+                <div class="key key-51">51. KEY_C</div>
+                <div class="key key-52">52. KEY_V</div>
+                <div class="key key-53">53. KEY_B</div>
+                <div class="key key-54">54. KEY_N</div>
+                <div class="key key-55">55. KEY_M</div>
+                <div class="key key-56">56. KEY_COMMA</div>
+                <div class="key key-57">57. KEY_DOT</div>
+                <div class="key key-58">58. KEY_SLASH</div>
+                <div class="key key-59">59. KEY_ARROW_UP</div>
+                <div class="key key-60">60. KEY_CTRL</div>
+                <div class="key key-61">61. KEY_SPACE</div>
+                <div class="key key-62">62. KEY_ALT</div>
+                <div class="key key-63">63. KEY_ARROW_LEFT</div>
+                <div class="key key-64">64. KEY_ARROW_DOWN</div>
+                <div class="key key-65">65. KEY_ARROW_RIGHT</div>
+*/
+
 
 $(document).ready(function() {
 
@@ -782,4 +873,153 @@ $(document).ready(function() {
             
     setup(sizeX, sizeY, sizePix, canvasLevelCount);
     setTimeout("simulationLoop()", 1000.0/parseFloat($('#sim-fps').val()));
+    
+    
+    var keypressed = false;
+    var nowOnStart = (new Date()).getTime();
+    
+    
+    // http://www.javascripter.net/faq/keycodes.htm
+    // http://stackoverflow.com/questions/8562528/is-there-is-a-way-to-detect-which-side-the-alt-key-is-pressed
+    
+    /*
+     delay rate  : 500 ms        (lub 250, 500, 750, 1000 ms)
+     repeat rate : 10.9 chars/s = 0.092 ms
+    
+     (that is, the inter-character delay is (2 ^ B) * (D + 8) / 240 sec, where B gives Bits 4-3 and D gives Bits 2-0).
+    
+     max is:  30.0 chars/s = 0.033 ms
+     min is:   2.0 chars/s = 0.500 ms
+                0         1         2         3         4         5         6         7
+      0      30.0      26.7      24.0      21.8      20.0      18.5      17.1      16.0
+      8      15.0      13.3      12.0      10.9      10.0       9.2       8.6       8.0
+      16      7.5       6.7       6.0       5.5       5.0       4.6       4.3       4.0
+      24      3.7       3.3       3.0       2.7       2.5       2.3       2.1       2.0
+    
+    */
+    /*
+
+  8      Backspace
+  9      Tab
+ 13      Enter
+ 16      Shift
+ 17      Ctrl
+ 18      Alt
+ 19      Pause, Break              [x]
+ 20      CapsLock
+ 27      Esc
+ 32      Space
+ 33      Page Up
+ 34      Page Down
+ 35      End
+ 36      Home
+ 37      Left arrow
+ 38      Up arrow
+ 39      Right arrow
+ 40      Down arrow
+ 44      PrntScrn (see below†)     [x]
+ 45      Insert
+ 46      Delete
+ 48-57   0 to 9
+ 65-90   A to Z  
+112-123  F1 to F12                 [x]
+144      NumLock                   [x]
+145      ScrollLock                [x]
+
+188      , <
+190      . >
+191      / ?
+192      ` ~
+219      [ { (see below‡)
+220      \ |
+221      ] }
+222      ' "
+    
+Opera   MSIE  Firefox  Safari  Chrome    Key pressed
+ 59     186      59     186     186      ; :   
+ 61     187     107     187     187      = +             
+109     189     109     189     189      - _     
+    
+Opera    All Others (NumLock On/Off)     Key pressed
+_________________________________________________________
+
+48/45       96/45                        Numpad 0 Ins    
+49/35       97/35                        Numpad 1 End    
+50/40       98/40                        Numpad 2 Down   
+51/34       99/34                        Numpad 3 Pg Down
+52/37      100/37                        Numpad 4 Left   
+53/12      101/12                        Numpad 5        
+54/39      102/39                        Numpad 6 Right  
+55/36      103/36                        Numpad 7 Home   
+56/38      104/38                        Numpad 8 Up     
+57/33      105/33                        Numpad 9 Pg Up  
+42/42      106/106                       Numpad *        
+43/43      107/107                       Numpad +        
+45/45      109/109   (45 is also Ins)    Numpad -        
+78/46      110/46    (78 is also N)      Numpad . Del    
+47/47      111/111                       Numpad /     
+
+     */
+    $(document).keydown(function (e) {
+        // logic for key event here
+        var now = (new Date()).getTime();
+        
+        if (e.keyCode==27) {
+            if (keyboardState[0].pressed!=true) {
+                keyboardState[0].pressed = true;
+                console.log( keyboardState[0] );
+            }
+        }
+        if (e.keyCode==192) {
+            if (keyboardState[1].pressed!=true) {
+                keyboardState[1].pressed = true;
+                console.log( keyboardState[1] );
+            }
+        }
+        
+        /*
+        if (e.keyCode==32) {
+            if (keypressed==false) {
+            
+                squareClick(0, 1, 0, 7, 1, 1, true, false); //toogle
+                keypressed = true;
+            }
+        }
+        if (console)
+            console.log('PRESS  : ' + e.keyCode + ', ' +(now - nowOnStart));
+        */
+        
+        e.preventDefault();
+    });
+    $(document).keyup(function (e) {
+        // logic for key event here
+        var now = (new Date()).getTime();
+        
+        
+        if (e.keyCode==27) {
+            if (keyboardState[0].pressed!=false) {
+                keyboardState[0].pressed = false;
+                console.log( keyboardState[0] );
+            }
+        }
+        if (e.keyCode==192) {
+            if (keyboardState[1].pressed!=false) {
+                keyboardState[1].pressed = false;
+                console.log( keyboardState[1] );
+            }
+        }
+        
+        /*
+        if (e.keyCode==32) {
+            if (keypressed==true) {
+                
+                squareClick(0, 1, 0, 7, 1, 1, true, false); //toogle
+                keypressed = false;
+            }
+        }
+        if (console)
+            console.log('______ : '+e.keyCode + ', ' + (now - nowOnStart));
+        */
+        e.preventDefault();
+    });
 });
